4ff7839122090a9c399210adf7a33e9b984b708d
Karl George <karlgdev@gmail.com>
2023-05-05 16:05:35 +0200

    Adding vendor schema

---
+++ sanity/vendor.js
@@ -0,0 +1,21 @@
+export default {
+  name: 'vendor',
+  title: 'Vendor',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+  ],
+};
